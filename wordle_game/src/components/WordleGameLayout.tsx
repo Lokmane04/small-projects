44a291bb706fa7b3b47d7bced7e019081f532276
ReD <baslilokmane04@gmail.com>
2023-12-08 00:57:07 +0100

    feat: Implement main container and child div elements for Wordle game layout

---
+++ wordle_game/src/components/WordleGameLayout.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const WordleGameLayout = () => {
+  return (
+    <main>
+      <span className="logo">Wordle</span>
+      <div className="container">
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child"></div>
+      </div>
+    </main>
+  );
+};
+
+export default WordleGameLayout;
