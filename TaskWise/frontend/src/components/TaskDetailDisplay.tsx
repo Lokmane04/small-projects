3127bde32973a30bdc96bae0d8a9c442d313864e
ReD <baslilokmane04@gmail.com>
2024-03-13 18:12:25 +0100

    feat : adding delete functionalities

--- TaskWise/frontend/src/components/TaskDetailDisplay.tsx
+++ TaskWise/frontend/src/components/TaskDetailDisplay.tsx
@@ -1,92 +1,129 @@
-import { useEffect, useState, Fragment } from "react";
+import { useEffect, useState, Fragment, useMemo } from "react";
 import axios from "axios";
 import { formaDataTypes } from "../types/index";
 import { Box, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 const TaskDetailDisplay = () => {
-  const [tasks, setTasks] = useState<formaDataTypes[] | undefined>([]);
-  useEffect(() => {
-    axios.get("http://localhost:3001").then((tasks) => {
-      setTasks(tasks.data);
-    });
-  }, []);
+  const [tasks, setTasks] = useState<formaDataTypes[]>([]);
 
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001");
+      setTasks(response.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  };
+
+  const handleDeleteTask = async (taskId: string | undefined) => {
+    try {
+      await axios.delete(`http://localhost:3001/deleteTask/${taskId}`);
+      await fetchData();
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [tasks]);
+  const memoizedTasks = useMemo(() => tasks, [tasks]);
   return (
     <>
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Title</th>
-            <th className="px-4 py-2">Description</th>
-            <th className="px-4 py-2">priority</th>
-            <th className="px-4 py-2">completed</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks?.map((task, i) => (
-            <Fragment key={i}>
-              {i % 2 === 1 ? (
-                <tr>
-                  <td className="border px-4 py-2">{task.title}</td>
-                  <td className="border px-4 py-2 w-full min-h-fit">
-                    {task.description}
-                  </td>
-                  <td className="border px-4 py-2">{task.priority}</td>
-                  <td className="border px-4 py-2">
-                    {task.completed ? "Done" : "Not yet"}
-                  </td>
-                  <td className="border px-4 py-2">
-                    <Link to={`/update/${task._id}`}>
-                      <Button
-                        color="info"
-                        variant="outlined"
-                        size="small"
-                        sx={{ mb: "10px" }}
-                      >
-                        Edit
-                      </Button>
-                    </Link>
-                    <Link to={`/delete/${task._id}`}>
-                      <Button color="error" variant="outlined" size="small">
-                        Delete
-                      </Button>
-                    </Link>
-                  </td>
-                </tr>
-              ) : (
-                <tr className="bg-gray-100">
-                  <td className="border px-4 py-2">{task.title}</td>
-                  <td className="border px-4 py-2 w-full min-h-fit">
-                    {task.description}
-                  </td>
-                  <td className="border px-4 py-2">{task.priority}</td>
-                  <td className="border px-4 py-2">
-                    {task.completed ? "Done" : "Not yet"}
-                  </td>
-                  <td className="border px-4 py-2">
-                    <Link to={`/update/${task._id}`}>
-                      <Button
-                        color="info"
-                        variant="outlined"
-                        size="small"
-                        sx={{ mb: "10px" }}
-                      >
-                        Edit
-                      </Button>
-                    </Link>
-                    <Link to={`/delete/${task._id}`}>
-                      <Button color="error" variant="outlined" size="small">
-                        Delete
-                      </Button>
-                    </Link>
-                  </td>
-                </tr>
-              )}
-            </Fragment>
-          ))}
-        </tbody>
-      </table>
-      <Box sx={{ width: "100%", paddingTop: "15px", margin: "0 40%" }}>
+      {memoizedTasks ? (
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Title</th>
+              <th className="px-4 py-2">Description</th>
+              <th className="px-4 py-2">priority</th>
+              <th className="px-4 py-2">completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {memoizedTasks &&
+              memoizedTasks?.map((task: formaDataTypes, i: number) => {
+                return (
+                  <Fragment key={i}>
+                    {i % 2 === 1 ? (
+                      <tr>
+                        <td className="border px-4 py-2">{task.title}</td>
+                        <td className="border px-4 py-2 w-full min-h-fit">
+                          {task.description}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {task.completed ? "Done" : "Not yet"}
+                        </td>
+                        <td className="border px-4 py-2">
+                          <Link to={`/update/${task._id}`}>
+                            <Button
+                              color="info"
+                              variant="outlined"
+                              size="small"
+                              sx={{ mb: "10px" }}
+                            >
+                              Edit
+                            </Button>
+                          </Link>
+                          <Button
+                            onClick={() => handleDeleteTask(task._id)}
+                            color="error"
+                            variant="outlined"
+                            size="small"
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    ) : (
+                      <tr className="bg-gray-100">
+                        <td className="border px-4 py-2">{task.title}</td>
+                        <td className="border px-4 py-2 w-full min-h-fit">
+                          {task.description}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {task.completed ? "Done" : "Not yet"}
+                        </td>
+                        <td className="border px-4 py-2">
+                          <Link to={`/update/${task._id}`}>
+                            <Button
+                              color="info"
+                              variant="outlined"
+                              size="small"
+                              sx={{ mb: "10px" }}
+                            >
+                              Edit
+                            </Button>
+                          </Link>
+                          <Button
+                            onClick={() => handleDeleteTask(task._id)}
+                            color="error"
+                            variant="outlined"
+                            size="small"
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    )}
+                  </Fragment>
+                );
+              })}
+          </tbody>
+        </table>
+      ) : (
+        <h1 className="my-16 font-bold text-5xl text-center">
+          Create your first task
+        </h1>
+      )}
+      <Box
+        sx={{
+          width: "100%",
+          paddingTop: "15px",
+          marginLeft: "45%",
+          marginRight: "45%",
+        }}
+      >
         <Link to={"/create"}>
           <Button color="primary" size="medium" variant="contained">
             Add New +
--- TaskWise/frontend/src/types/index.ts
+++ TaskWise/frontend/src/types/index.ts
@@ -1,25 +1,14 @@
-import { ZodBoolean, ZodDefault, ZodEnum, ZodObject, ZodString, z } from "zod";
-
-export interface formaDataTypes {
+export type formaDataTypes = {
+  description: string;
   _id?: string;
-  title: string;
-  description?: string;
-  completed: boolean;
-  priority: "low" | "medium" | "high" | "delayed"; // you can modify this field depending on your needs
-  __v?: number;
-}
-export const taskSchema = z.object({
-  title: z
-    .string()
-    .min(3, { message: "the title must be at least 3 caracteres" })
-    .max(20, { message: "the title must be less than 3 caracteres" }),
-  description: z.string(),
-  completed: z.boolean().default(false),
-  priority: z.enum(["low", "medium", "high", "delayed"]),
-});
-export const taskType = ZodObject<{
-  title: ZodString;
-  description: ZodString;
-  completed: ZodDefault<ZodBoolean>;
-  priority: ZodEnum<["low", "medium", "high", "delayed"]>;
-}>;
+  title?: string;
+  completed?: boolean;
+};
+
+// After
+// export type formaDataTypes = {
+//   description: string | undefined;
+//   _id?: string;
+//   title?: string | undefined; // Adjusted to allow undefined
+//   completed?: boolean | undefined;
+// };
